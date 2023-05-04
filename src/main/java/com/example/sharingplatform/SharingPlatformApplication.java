207e1b334b501dd3362877fe4a1541e1dcadb91d
microne <2647293427@qq.com>
2023-05-04 22:59:30 +0800

    initial

---
+++ src/main/java/com/example/sharingplatform/SharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sharingplatform/SharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
