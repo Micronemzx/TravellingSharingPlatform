97d69053b594018e8277d58fca6c40a7a9a58a6f
microne <2647293427@qq.com>
2023-05-08 00:07:24 +0800

    initial

--- src/main/java/com/example/sharingplatform/entity/user.java
+++ src/main/java/com/example/sharingplatform/entity/user.java
@@ -14,8 +14,88 @@ public class user {
     private String email;
     private String mailcode;
     private String gender;
-    private String phoneNumbee;
+    private String phoneNumber;
     private Date birth;
     private String photoPath;
     private int workNumber;
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMailcode() {
+        return mailcode;
+    }
+
+    public void setMailcode(String mailcode) {
+        this.mailcode = mailcode;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public int getWorkNumber() {
+        return workNumber;
+    }
+
+    public void setWorkNumber(int workNumber) {
+        this.workNumber = workNumber;
+    }
 }
---
+++ src/main/java/com/example/sharingplatform/entity/work.java
@@ -0,0 +1,103 @@
+package com.example.sharingplatform.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Table(name="work")
+@Entity
+public class work {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long workID;
+    private String title;
+    private String content;
+    private String[] picture;
+    private String userName;
+    private long userID;
+    private Date createTime;
+    private String place;
+    private long likeNumber;
+    private long commentNumber;
+    private int authority;
+
+    public long getWorkID() {
+        return workID;
+    }
+
+    public void setWorkID(long workID) {
+        this.workID = workID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public long getLikeNumber() {
+        return likeNumber;
+    }
+
+    public void setLikeNumber(long likeNumber) {
+        this.likeNumber = likeNumber;
+    }
+
+    public long getCommentNumber() {
+        return commentNumber;
+    }
+
+    public void setCommentNumber(long commentNumber) {
+        this.commentNumber = commentNumber;
+    }
+
+    public int getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(int authority) {
+        this.authority = authority;
+    }
+}
